95186fdecc4eacad9c9a6c83247b52cf201c9721
Rodrigo <rodrigo.hammes7@gmail.com>
2020-04-19 00:22:05 -0300

    Implementa verificacoes de upload de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -32,9 +33,18 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file.filename);
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
 
-    return response.json({ ok: true });
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 export default usersRouter;
