47a8153fb9a50c472b7a8f73e94e3d79e4f12959
Rodrigo <rodrigo.hammes7@gmail.com>
2020-05-09 13:55:43 -0300

    Instala e configura o Jest para os testes

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,12 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(1+2).toBe(3)
+  });
+
+  it('should not be able to create a two appointments on the same time', () => {
+    expect(1+2).toBe(3)
+  });
+
+});
