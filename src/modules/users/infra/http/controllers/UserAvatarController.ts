25afd2945f2c45dc787a0cb39c27de6dec88a153
Rodrigo <rodrigo.hammes7@gmail.com>
2020-07-04 22:48:05 -0300

    Implementa class-transformer

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,11 +1,11 @@
-import { Request, Response} from 'express';
+import { Request, Response } from 'express';
+import { classToClass } from 'class-transformer';
 import { container } from 'tsyringe';
 
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
-export default class UserAvatarController{
-  public async update(request: Request ,response: Response): Promise<Response>{
-
+export default class UserAvatarController {
+  public async update(request: Request, response: Response): Promise<Response> {
     const updateUserAvatar = container.resolve(UpdateUserAvatarService);
 
     const user = await updateUserAvatar.execute({
@@ -13,6 +13,6 @@ export default class UserAvatarController{
       avatarFilename: request.file.filename,
     });
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
